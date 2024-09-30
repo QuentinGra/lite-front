24a11fea6ac34fed5c214924887212fbc154a492
QuentinGra <pilou4260042@gmail.com>
2024-09-30 17:13:06 +0200

    feat(category): add search input and style

--- src/views/admin/category/AdminCategoryView.vue
+++ src/views/admin/category/AdminCategoryView.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import CategoryList from '@/components/admin/CategoryList.vue'
+
+const searchQuery = ref('')
 </script>
 
 <template>
   <div>
     <h1>Liste des catégories</h1>
+    <label for="search-input" class="search-label">Rechercher une catégorie</label>
     <div class="button-container">
+      <input type="text" v-model="searchQuery" placeholder="Nom" class="search-input" />
       <router-link to="/admin-panel/categorie/creer" class="button-create">Créer</router-link>
     </div>
     <CategoryList />
