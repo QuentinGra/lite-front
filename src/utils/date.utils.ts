a1c6c8a21bcf1a841db37a59deab0f9fcc48dd3e
QuentinGra <pilou4260042@gmail.com>
2024-10-22 17:51:37 +0200

    refactor(dateUtils): add French locale date formatting function

--- src/utils/date.utils.ts
+++ src/utils/date.utils.ts
@@ -1,3 +1,9 @@
+/**
+ * Formats a given date into a string with the format "day month year" in French locale.
+ *
+ * @param dateString - The date to be formatted.
+ * @returns The formatted date string in "day month year" format.
+ */
 export const formatDate = (dateString: Date): string => {
   const date = new Date(dateString)
   return date.toLocaleDateString('fr-FR', {
--- src/utils/string.utils.ts
+++ src/utils/string.utils.ts
@@ -1,3 +1,9 @@
+/**
+ * Capitalizes the first letter of a given string.
+ *
+ * @param string - The string to capitalize.
+ * @returns The input string with the first letter capitalized.
+ */
 export const capitalizeFirstLetter = (string: string): string => {
   return string.charAt(0).toUpperCase() + string.slice(1)
 }
