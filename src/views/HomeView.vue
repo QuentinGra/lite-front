a4d1681a25fe4b620fcba8614c051ac1fa6f7a7f
QuentinGra <pilou4260042@gmail.com>
2024-11-07 22:15:55 +0100

    feat(home): enhance HomeView with presentation content and structure

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// import LatestBooks from '@/components/public/books/LatestBooks.vue'
+</script>
 
 <template>
-  <section>
-    <h1>Home page</h1>
+  <section class="container">
+    <div class="home-presentation">
+      <h1 class="home-title">Bienvenue sur Lite</h1>
+      <p class="home-description">
+        Découvrez Lite, votre nouvelle plateforme de partage littéraire. Partagez vos impressions,
+        découvrez de nouveaux livres et rejoignez une communauté passionnée de lecteurs.
+      </p>
+      <p class="home-description">
+        Que vous soyez amateur de romans, de poésie ou d'essais, Lite vous permet de garder une
+        trace de vos lectures et d'échanger avec d'autres passionnés.
+      </p>
+    </div>
+    <!-- TODO Components LatestBooks -->
   </section>
 </template>
