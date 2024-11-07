150580bc189c065652b96b158e7fb925e45c7438
QuentinGra <pilou4260042@gmail.com>
2024-11-07 20:08:19 +0100

    chore: remove vite-svg-loader and associated type definitions

--- src/types/svg.d.ts
+++
@@ -1,5 +0,0 @@
-declare module '*.svg' {
-  import type { ComponentOptions } from 'vue'
-  const component: ComponentOptions
-  export default component
-}
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import mkcert from 'vite-plugin-mkcert'
 import Markdown from 'unplugin-vue-markdown/vite'
-import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -22,8 +21,7 @@ export default defineConfig({
         linkify: true,
         typographer: true
       }
-    }),
-    svgLoader()
+    })
   ],
   resolve: {
     alias: {
